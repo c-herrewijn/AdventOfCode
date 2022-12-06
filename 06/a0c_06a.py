f0562ca45a86187358c2a3407117d066b89edf86
Christiaan Herrewijn <c.herrewijn@gmail.com>
2022-12-06 09:55:13 +0100

    remove superfluous line

--- 06/a0c_06a.py
+++ 06/a0c_06a.py
@@ -2,7 +2,6 @@ found = False
 count = 0
 with open("06/input.txt") as file:
     s = file.read()
-    sub = s[count:count+4]
     while not found:
         sub = s[count:count+4]
         found = (len(set(sub)) == len(sub))
