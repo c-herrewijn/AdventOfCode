578c2d41521a67725ccf4f523f1d84aa150e2eac
Christiaan Herrewijn <c.herrewijn@gmail.com>
2022-12-06 09:16:35 +0100

    added day 6

---
+++ 06/a0c_06b.py
@@ -0,0 +1,12 @@
+found = False
+count = 0
+with open("06/input.txt") as file:
+    s = file.read()
+    sub = s[count:count+4]
+    while not found:
+        sub = s[count:count+14]
+        found = (len(set(sub)) == len(sub))
+        count+=1
+
+# result
+print(count+13)
