2fbfc189cee7a9e50bb21ed7316a8c6a6d2779ab
Christiaan Herrewijn <cherrewi@f0r2s7.codam.nl>
2022-12-08 18:00:41 +0100

    removed superfluous parantheses

--- 08/a0c_08b.py
+++ 08/a0c_08b.py
@@ -1,18 +1,18 @@
 def v_north(f, x, y):
     y_coord_block = next((i for i in range(y-1, 0, -1) if (f[i][x] >= f[y][x])), 0)
-    return(y - y_coord_block)
+    return y - y_coord_block
 
 def v_south(f, x, y, y_max):
     y_coord_block = next((i for i in range(y+1, y_max) if (f[i][x] >= f[y][x])), y_max - 1)
-    return(y_coord_block - y)
+    return y_coord_block - y
 
 def v_west(f, x, y):
     x_coord_block = next((i for i in range(x-1, 0, -1) if (f[y][i] >= f[y][x])), 0)
-    return(x - x_coord_block)
+    return x - x_coord_block
 
 def v_east(f, x, y, x_max):
     x_coord_block = next((i for i in range(x+1, x_max) if (f[y][i] >= f[y][x])), x_max - 1)
-    return(x_coord_block - x)
+    return x_coord_block - x
 
 def scenic_score(f, x, y, x_max, y_max):
     return v_north(f, x, y) * v_south(f, x, y, y_max) * v_west(f, x, y) * v_east(f, x, y, x_max)
