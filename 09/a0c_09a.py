54f33c5d139d01b4e03b3c217d6eb982344a87c0
Christiaan Herrewijn <cherrewi@f0r2s7.codam.nl>
2022-12-09 16:17:05 +0100

    used tuple for coordinates; compressed code

--- 09/a0c_09a.py
+++ 09/a0c_09a.py
@@ -1,31 +1,24 @@
 def move_head(dir, h):
     dirs = {'U': (0, 1), 'D': (0, -1), 'R': (1, 0), 'L': (-1, 0)}
-    h = [h[i] + dirs[dir][i] for i in range(2)]
-    return h
+    return (h[0] + dirs[dir][0], h[1] + dirs[dir][1])
 
 
 def move_tail(h, t):
-    if abs(h[0] - t[0]) == 2 or abs(h[1] - t[1]) == 2:
-        if abs(h[0] - t[0]) == 2:
-            t[0] = int((h[0] + t[0]) / 2)
-        else:
-            t[0] = h[0]
-        if abs(h[1] - t[1]) == 2:
-            t[1] = int((h[1] + t[1]) / 2)
-        else:
-            t[1] = h[1]
-    return t
+    x_new = (int((h[0] + t[0]) / 2) if abs(h[0] - t[0]) == 2 else h[0])
+    y_new = (int((h[1] + t[1]) / 2) if abs(h[1] - t[1]) == 2 else h[1])
+    return (x_new, y_new)
 
 
-h = [0, 0]
-t = [0, 0]
+h = (0, 0)
+t = (0, 0)
 trail = {(0, 0)}
 with open("09/input.txt") as file:
     for line in file:
         dir, steps = line.strip().split()
         for _ in range(int(steps)):
             h = move_head(dir, h)
-            t = move_tail(h, t)
-            trail.add(tuple(t))
+            if abs(h[0] - t[0]) == 2 or abs(h[1] - t[1]) == 2:
+                t = move_tail(h, t)
+            trail.add(t)
 
 print(len(trail))
--- 09/a0c_09b.py
+++ 09/a0c_09b.py
@@ -1,23 +1,15 @@
 def move_head(dir, h):
     dirs = {'U': (0, 1), 'D': (0, -1), 'R': (1, 0), 'L': (-1, 0)}
-    h = [h[i] + dirs[dir][i] for i in range(2)]
-    return h
+    return (h[0] + dirs[dir][0], h[1] + dirs[dir][1])
 
 
 def move_knot(h, t):
-    if abs(h[0] - t[0]) == 2 or abs(h[1] - t[1]) == 2:
-        if abs(h[0] - t[0]) == 2:
-            t[0] = int((h[0] + t[0]) / 2)
-        else:
-            t[0] = h[0]
-        if abs(h[1] - t[1]) == 2:
-            t[1] = int((h[1] + t[1]) / 2)
-        else:
-            t[1] = h[1]
-    return t
+    x_new = (int((h[0] + t[0]) / 2) if abs(h[0] - t[0]) == 2 else h[0])
+    y_new = (int((h[1] + t[1]) / 2) if abs(h[1] - t[1]) == 2 else h[1])
+    return (x_new, y_new)
 
 
-knots = [[0, 0] for _ in range(10)]
+knots = [(0, 0) for _ in range(10)]
 trail = {(0, 0)}
 with open("09/input.txt") as file:
     for line in file:
@@ -25,7 +17,8 @@ with open("09/input.txt") as file:
         for _ in range(int(steps)):
             knots[0] = move_head(dir, knots[0])
             for i in range(1, 10):
-                knots[i] = move_knot(knots[i-1], knots[i])
-            trail.add(tuple(knots[9]))
+                if abs(knots[i-1][0] - knots[i][0]) == 2 or abs(knots[i-1][1] - knots[i][1]) == 2:
+                    knots[i] = move_knot(knots[i-1], knots[i])
+            trail.add(knots[9])
 
 print(len(trail))
