c602a4ac683954f718e8d7b92e25da1c1f300cd3
Christiaan Herrewijn <c.herrewijn@gmail.com>
2022-12-13 19:06:05 +0100

    added compare function

--- 13/a0c_13a.py
+++ 13/a0c_13a.py
@@ -39,8 +39,41 @@ def parse_line(line):
     return lst
 
 
+'''
+left and right can be integer, list or mixed
+'''
+def compare(left, right):
+    if (type(left) is not list) and (type(right) is not list):
+        if left < right:
+            return 1  # correct order
+        elif left > right:
+            return -1  # wrong order
+        else:
+            return 0  # undecided
+    if type(left) is not list:
+        left = [left]
+    if type(right) is not list:
+        right = [right]
+    for i, _ in enumerate(left):
+        if i > len(right) - 1:
+            return -1
+        result = compare(left[i], right[i])
+        if result != 0:
+            return result
+    return 1
+
+
+sum_indices = 0
+index = 1
 with open("13/input_test.txt") as file:
     lines = file.readlines()
     for i in range(0, len(lines), 3):
-        left = lines[i].strip()
-        right = lines[i+1].strip()
+        left = parse_line(lines[i].strip())
+        right = parse_line(lines[i+1].strip())
+        if compare(left, right) == 1:
+            sum_indices += index
+        index += 1
+
+print(sum_indices)
+
+# 6251 -> too high!
