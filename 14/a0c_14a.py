ede2a5fbe3fd32bbca668bc96e682ae90d2f0ab0
Christiaan Herrewijn <c.herrewijn@gmail.com>
2022-12-14 23:50:52 +0100

    day 14 - parse file

--- 14/a0c_14a.py
+++ 14/a0c_14a.py
@@ -8,15 +8,32 @@ def min_max_input(instructions):
 
 # returns list with x,y tuples
 def parse_line(line):
-    return [tuple(point.split(",")) for point in line.strip().split(" -> ")]
+    return [tuple(map(int, point.split(","))) for point in line.strip().split(" -> ")]
 
 
-field = []
+def print_field(field):
+    for y in range(y_max + 1):
+        for x in range(x_min, x_max + 1):
+            print(field[y][x-x_min], end="")
+        print()
+
+
+def add_rock_to_field(r_start, r_end):
+    x_coords = (r_start[0]-x_min, r_end[0]-x_min)
+    y_coords = (r_start[1], r_end[1])
+    for x in range(min(x_coords), max(x_coords)+1):
+        for y in range(min(y_coords), max(y_coords)+1):
+            field[y][x] = "#"
+
+# parse file
 with open("14/input_test.txt") as file:
     instructions = [parse_line(line) for line in file.readlines()]
-    x_min, x_max, y_min, y_max = min_max_input(instructions)
 
-    print(x_min)
-    print(x_max)
-    print(y_min)
-    print(y_max)
+# create field
+x_min, x_max, y_min, y_max = min_max_input(instructions)
+field = [["." for _ in range(x_min, x_max+1)] for _ in range(y_max+1)]
+for line in instructions:
+    for i in range(len(line)-1):
+        add_rock_to_field(line[i], line[i+1])
+
+print_field(field)
